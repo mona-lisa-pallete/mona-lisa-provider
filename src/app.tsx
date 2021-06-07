c5a90a9b59283aea7252f7c2ddfcc19295530b25
sanget <zh.sanget@gmail.com>
2021-06-07 15:16:42 +0800

    feat: 添加 sentry

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { ConfigProvider } from 'antd';
+
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+
 import zhCN from 'antd/es/locale/zh_CN';
 import './global.less';
 
@@ -10,6 +14,19 @@ import MaterialLibraryContainer from './components/MaterialLibraryContainer/';
  * 注册 React 运行环境，模拟由 sdk 构建的 dll 环境，以后将要删掉
  */
 window.React = React;
+
+/**
+ * 注册 sentry
+ */
+Sentry.init({
+  dsn: 'https://4072071bc4a14619b507c556d3477f9d@o807559.ingest.sentry.io/5803649',
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 moment.locale('zh-cn');
 
